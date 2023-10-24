846314cdf71242c2863f4eebeb6ef1b18d001a73
gangster <asliddinkarimboyev@gmail.com>
2023-10-24 18:36:04 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './common/config/env.config';
-import * as hash from './common/util/hash.util';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(env.PORT, () => {
     console.log(`server running on http://127.0.0.1:${env.PORT}`);
